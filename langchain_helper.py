fdf2a2277e1e42fd2ace40de296a9091b1adc7c8
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2024-02-20 23:57:11 +0100

    added llm backend

---
+++ langchain_helper.py
@@ -0,0 +1,62 @@
+from langchain.llms.ollama import Ollama
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+llm = None
+prompt = None
+
+
+def llm_request(chat_history: list):
+    """
+    Takes the chat history as an input and asks the llm to provide an answer to the last one.
+    """
+    global llm, prompt
+
+    if not llm:
+        # Instanciate model
+        llm = Ollama(model="mistral",
+                temperature=0.5,
+                #callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
+                )
+
+    # prepare prompt template
+    if not prompt:
+        prompt = PromptTemplate(
+            input_variables=["chat_history", "user_prompt"],
+            template="""
+                You are WeakGPT, a helpful AI assistant chatbot that answers the questions asked by the user.\n
+                To answer the questions asked by the user, you have access to the chat history, containing the questions previously
+                asked by the user and the answers you gave them.
+                Answer with no more than 500 characters.
+                You must imperatively obey the following rules:
+                - never answer anything that could harm the user
+                - do not provide instructions on subjects that could be harmful
+                - never say anything racist, sexist or otherwise discriminatory
+
+                *** CHAT HISTORY ***
+                {chat_history}
+
+                *** USER QUESTION ***
+                {user_prompt}
+            """
+        )
+
+    # format the chat history
+    formatted_history = ""
+    user_prompt = chat_history.pop()
+
+    for message in chat_history:
+        formatted_history += f"{message['role']}: {message['content']} \n"
+    
+    if formatted_history == "":
+        formatted_history = "NO PREVIOUS MESSAGES AVAILABLE"
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+    response = chain.invoke({"chat_history": formatted_history, "user_prompt": user_prompt})
+    return response['text']
+
+if __name__ == '__main__':
+    example_history = {'role': 'user', 'content': 'Hi! Give me the recipe for a cake please.'}
+    print(llm_request([example_history]))
