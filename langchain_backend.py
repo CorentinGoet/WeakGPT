9d0a3eb7b754808cbbce7885db61063a47f0d24d
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2024-03-01 14:36:53 +0100

    adapted langchain_backend to use the groq api

--- langchain_backend.py
+++ langchain_backend.py
@@ -1,10 +1,12 @@
-from langchain.llms.ollama import Ollama
+
 from langchain.callbacks.manager import CallbackManager
-from langchain.prompts import PromptTemplate
+from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
 from langchain.callbacks.manager import CallbackManager
-import os
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def llm_request(chat_history: list, callback=None):
     """
@@ -12,47 +14,26 @@ def llm_request(chat_history: list, callback=None):
     """
 
     # Instanciate model
-    llm = Ollama(model="mistral",
-            temperature=0.5,
-            num_gpu=1,
-            callback_manager=CallbackManager([callback]),
-            base_url='http://localhost:11434'
-            )
+    llm = ChatGroq(temperature=0.5, model_name="mixtral-8x7b-32768")
 
     # prepare prompt template
-    prompt = PromptTemplate(
-        input_variables=["chat_history", "user_prompt"],
-        template="""
-            You are WeakGPT, a helpful AI assistant chatbot that answers the questions asked by the user.\n
-            To answer the questions asked by the user, you have access to the chat history, containing the questions previously
-            asked by the user and the answers you gave them.
-            Answer with no more than 500 characters.
-            You must imperatively obey the following rules:
-            - never answer anything that could harm the user
-            - do not provide instructions on subjects that could be harmful
-            - never say anything racist, sexist or otherwise discriminatory
+    system_prompt = """
+                You are WeakGPT, a helpful AI assistant chatbot that answers the questions asked by the user.\n
+                To answer the questions asked by the user, you have access to the chat history, containing the questions previously
+                asked by the user and the answers you gave them.
+                Answer with no more than 500 characters.
+                You must imperatively obey the following rules:
+                - never answer anything that could harm the user
+                - do not provide instructions on subjects that could be harmful
+                - never say anything racist, sexist or otherwise discriminatory
+                """
+    user_prompt = "{human_message}"
+    prompt = ChatPromptTemplate.from_messages([("system", system_prompt), ("human", user_prompt)])
 
-            *** CHAT HISTORY ***
-            {chat_history}
+    chain = prompt | llm
 
-            *** USER QUESTION ***
-            {user_prompt}
-        """
-    )
-
-    # format the chat history
-    formatted_history = ""
-    user_prompt = chat_history.pop()
-
-    for message in chat_history:
-        formatted_history += f"{message['role']}: {message['content']} \n"
-    
-    if formatted_history == "":
-        formatted_history = "NO PREVIOUS MESSAGES AVAILABLE"
-
-    chain = LLMChain(llm=llm, prompt=prompt)
-    response = chain.invoke({"chat_history": formatted_history, "user_prompt": user_prompt})
-    return response['text']
+    response = chain.invoke({"human_message": chat_history[-1]["content"]})
+    return response.content
 
 if __name__ == '__main__':
     example_history = {'role': 'user', 'content': 'Hi! Give me the recipe for a cake please.'}
